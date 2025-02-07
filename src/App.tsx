a47d06f11451ad1b8c60ed2b696e5901d1ce57b8
abul <abulkhan19175@gmail.com>
2025-02-07 12:02:05 +0530

    Font, Name

--- src/App.tsx
+++ src/App.tsx
@@ -42,14 +42,20 @@ function App() {
 
   return (
     <div className="min-h-screen bg-black p-4">
+      <div className="max-w-5xl text-center mx-auto px-4 md:px-8 mb-8">
+        <h1 className="text-3xl md:text-5xl font-bold text-white relative inline-block">
+          Infinite Image Gallery
+          <span className="absolute -bottom-2 left-0 right-0 h-1 bg-gradient-to-r from-blue-500 to-purple-500"></span>
+        </h1>
+      </div>
       <FocusCards photos={photos} />
       {isLoading && (
-        <div className="text-center text-gray-600 mt-4 pb-4">
+        <div className="text-center text-gray-400 mt-4 pb-4">
           Loading more images...
         </div>
       )}
       {!hasMore && (
-        <div className="text-center text-gray-600 mt-4 pb-4">
+        <div className="text-center text-gray-400 mt-4 pb-4">
           No more images to load
         </div>
       )}
